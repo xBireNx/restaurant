8e17e62a3c5865dc2c28506391ef910b66e9138c
xBireNx <shashwat.biren007@gmail.com>
2023-10-09 21:40:08 +0530

    Forth Commit

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,5 @@
 import Card from '../UI/Card';
-import MealItem from '../MealItem/MealItem';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
